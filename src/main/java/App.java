6467613dba080f23844757dd2da32f9cafaf8e47
jocelyn alsdorf & mlmendeznunez & morgan lutz <student@epicodus.com>
2015-08-26 16:37:54 -0700

    adds routes for index and admin

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,10 +12,30 @@ public class App {
   get("/", (request, response) -> {
     HashMap<String, Object> model = new HashMap<String, Object>();
     //categories here can be anything as long as it matches $categories
-    // model.put("cuisines", Cuisine.all());
+    model.put("cuisines", Cuisine.all());
     model.put("template", "templates/index.vtl");
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
+
+  post("/cuisines", (request, response) -> { //post route must match form action
+    HashMap<String,Object> model = new HashMap<String, Object>();
+    String foodtype = request.queryParams("foodtype");
+    Cuisine newCuisine = new Cuisine(foodtype);
+    newCuisine.save();
+    model.put("cuisines", Cuisine.all());
+    //put arraylist of cuisines on page
+    model.put("template", "templates/index.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  get("/cuisines", (request, response) -> {
+  //need to put :id in the url so that we can grab it below
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    model.put("cuisines", Cuisine.all());
+    model.put("template", "templates/admin.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
 
 }//end of main
 
